4f044e65ede0bc2a1b178857b83e8d85bfc53910
KLPig <kkkkkent@qq.com>
2024-03-19 21:20:32 +0800

    Add keyboard support, update 404, score and timer

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 scr = document.getElementById('screen')
+ip = document.getElementById('input')
 
 blocks = new Array()
 id = -1
 lv = 0
 kuns = new Array()
-score = -5
+score = -50
+tm = 100
 
 function generate(){
-    score++;
+    tm += 10;
+    score += 10 + (lv - lv % 10);
     localStorage.score = score;
     lv++;
     for(var x = 0; x < blocks.length; x++){
@@ -22,6 +25,19 @@ function generate(){
         id++;
     }
 }
+
+ip.addEventListener(
+    'input', 
+    function(){
+        if(ip.value < 1 || ip.value > 4)
+            return;
+        if(ip.value - 1 == kuns[lv - 5])
+            generate()
+        else
+            tm -= 50
+        ip.value = ''
+    }
+)
 
 class Block{
     constructor(col, is_kun, id, row){
@@ -48,11 +64,13 @@ class Block{
         if(lv - self.r == 5){
             self.element.className += ' under'
             if(self.element.tagName == 'IMG')
-                self.element.addEventListener('click', function(){
-                    window.open('./score-show.htm', '_self')}
-                )
+                var func = function(){
+                    tm -= 50
+                }
             else
-                self.element.addEventListener('click', generate)
+                var func = generate
+            self.element.addEventListener('click', func)
+              
         }
         if(lv - self.r == 6){
             self.element.className = 'block'
@@ -69,13 +87,12 @@ for(var x = 0; x < 5; x++){
     setTimeout(generate, x * 100)
 }
 
-tm = 100
-
 setInterval(
     function(){
-        tm -= 1
+        tm -= 1 + (id - id % 100) / 100
         localStorage.time = tm
+        if(tm <= 0)
+            window.open('./score-show.htm', '_self')
     }, 100
 )
 
-setTimeout(function(){window.open('./score-show.htm', '_self')}, 10000)
